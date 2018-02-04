f481f3d70abce5732cf710cb8a760cdf5c307d08
NazeerBasha Shaik <bashashaik@live.in>
2018-02-04 13:14:35 +0530

    Initial commit, including first exercise.

---
+++ VidlyMVC/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMVC.Models;
+
+namespace VidlyMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public static List<Customer> lstCustomer = new List<Customer>() { new Customer() { Id = 1, Name = "Customer1" },
+                new Customer() { Id=2, Name = "Customer2"}};
+
+        public ActionResult Index()
+        {
+            return View(lstCustomer);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Customer objCustomer = lstCustomer.Single(x => x.Id == id);
+            return View(objCustomer);
+        }
+    }
+}
---
+++ VidlyMVC/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMVC.Models;
+
+namespace VidlyMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public static List<Movie> Movies = new List<Movie>() { new Movie() { Id = 1, Name = "Ninnu kori" }, new Movie() { Id = 2, Name = "MCA" } };
+
+        public ActionResult Index()
+        {
+            return View(Movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(Movies.Single(x => x.Id == id));
+        }
+    }
+}
